7952bdf687b55dc7d764382355dacfb57582c0f9
johannDeLaCruz <ioann.mendez@crossnetics.io>
2024-08-18 19:13:45 +0700

    feat(CasesPage): :bug: add page and router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,28 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-
 import App from './App.tsx'
+import * as React from 'react'
+import * as ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { CasesPage } from './pages/Cases'
+// import Contacts from './pages/Contacts'
 
-createRoot(document.getElementById('root')!).render(
-    <StrictMode>
-        <App />
-    </StrictMode>,
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App />,
+    },
+    {
+        path: '/cases',
+        element: <CasesPage />,
+    },
+    // {
+    //     path: '/contacts',
+    //     element: <Contacts />,
+    // },
+])
+
+const rootElement = document.getElementById('root')!
+ReactDOM.createRoot(rootElement).render(
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>,
 )
---
+++ src/pages/Cases/CasesPage.tsx
@@ -0,0 +1,9 @@
+import styles from './CasesPage.module.scss'
+
+export const CasesPage: React.FC = () => {
+    return (
+        <section className={styles.root} id='cases'>
+            hi
+        </section>
+    )
+}
