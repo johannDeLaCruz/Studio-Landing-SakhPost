9e770faa161616602ef51e2ae839826d98f3c53b
johannDeLaCruz <ioann.mendez@crossnetics.io>
2024-08-23 12:35:26 +0700

    feat(casesSection): :lipstick: add barebones fluid styles

--- src/components/CasesSection/CasesSection.tsx
+++ src/components/CasesSection/CasesSection.tsx
@@ -1,14 +1,25 @@
 import styles from './CasesSection.module.scss'
 import { CircleButton } from '@ui/CircleButton'
+import { cases } from './constants'
 
 export const CasesSection: React.FC = () => {
     return (
         <section className={styles.root} id='cases'>
             <h2>Лучшие работы</h2>
             <div className={styles.casesList}>
-                <div className={styles.caseCard}></div>
-                <div className={styles.caseCard}></div>
-                <div className={styles.caseCard}></div>
+                {cases.map(({ name, img, category }, index) => (
+                    <div key={index} className={styles.caseCard}>
+                        <img src={img} alt='case-image' />
+                        <div className={styles.content}>
+                            <div className={styles.category}>
+                                <h3>{category}</h3>
+                            </div>
+                            <div className={styles.name}>
+                                <p>{name}</p>
+                            </div>
+                        </div>
+                    </div>
+                ))}
             </div>
             <div className={styles.buttonWrapper}>
                 <CircleButton type='white'>Все кейсы</CircleButton>
---
+++ src/components/CasesSection/constants.ts
@@ -0,0 +1,27 @@
+import GarsonImage from '@assets/images/case-image-01.jpg'
+import SendanceImage from '@assets/images/case-image-02.jpg'
+import SakhPostImage from '@assets/images/case-image-03.jpg'
+import SilService from '@assets/images/case-image-04.jpg'
+
+export const cases = [
+    {
+        category: 'платформа',
+        name: 'GarsON',
+        img: GarsonImage,
+    },
+    {
+        category: 'лендинг',
+        name: 'Sendance',
+        img: SendanceImage,
+    },
+    {
+        category: 'Портал',
+        name: 'SakhPost',
+        img: SakhPostImage,
+    },
+    {
+        category: 'лендинг',
+        name: 'Sil Service',
+        img: SilService,
+    },
+]
